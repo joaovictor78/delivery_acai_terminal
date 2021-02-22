5c9ef42de51314f0e041e785a322b68db0ee852a
José Vitor Gonçalves de Melo <josemelo1904003@gmail.com>
2021-02-22 00:58:36 +0000

    tratamento de erro na funcionalidae de cadastro de usuário

--- pages/tela_cadastro.py
+++ pages/tela_cadastro.py
@@ -1,15 +1,16 @@
-import os, time
+import os
 from models import usuario
 from utils import validate
 
-def validaInputs(function):
+def validaInputs(message, function):
   while(True):
     try:
-      function
+      data = input(message)
+      function(data)
+      return data
       break
     except Exception as error:
       print(error)
-      return False
 
 def telaCadastro():
   os.system('cls' if os.name == 'nt' else 'clear')
@@ -18,27 +19,8 @@ def telaCadastro():
   print("Bem vindo a melhor acaiteria da cidade!")
   print("---------------------------------------")
   print("Cadastre-se para continuar!")
-  while(True):
-    nome = input("Digite seu nome: ")
-    if(validaInputs(nome, validate.validaName) == False):
-      continue
-    else:
-      while(True):
-        telefone = input("Digite seu Número:")
-        if(validaInputs(telefone, validate.validaTelefone) == False):
-          continue
-        else:
-          while(True):
-            email = input("Digite seu email: ")
-            if(validaInputs(email, validate.validaEmail) == False):
-              continue
-  senha = ""
-  while(True):
-    senha = input("Digite sua senha:")
-    confirmar_senha = input("Confirme sua senha:")
-    if(senha == confirmar_senha):
-      break
-    else:
-      os.system('cls' if os.name == 'nt' else 'clear')
-      print("As senhas não condizem digite novamente:/")
-  validaInputs(senha, validate.validaSenha)
+  nome = validaInputs("Digite seu nome: ", validate.validaName)
+  telefone = validaInputs("Digite seu Número: ", validate.validaTelefone)
+  email = validaInputs("Digite seu email: ", validate.validaEmail)
+  senha = validaInputs("Digite sua senha: ", validate.validaSenha)
+  usuario.Usuario().registrarConta(nome, email, senha, telefone)
